5ff7787a985afcc963320ef6a758dbf9cddca7b3
Pain <ijcasimbon@gmail.com>
2022-04-25 01:47:04 -0400

    modified footer again

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <footer class="py-5 bottom">
+      <footer class="py-5 fixed-bottom">
         <div class="container">
           <p class="m-0 text-center text-white">
-            Site was last update at April 23,2022.
+            Site was last update at April 25,2022.
           </p>
         </div>
       </footer>
