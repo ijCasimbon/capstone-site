d14c8cb550960192ab2000773f40678662bd76ea
Pain <ijcasimbon@gmail.com>
2022-04-24 22:12:34 -0400

    made leader board page

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -28,6 +28,11 @@ function Navigation() {
                 </NavLink>
               </li>
               <li className="nav-item">
+                <NavLink className="nav-link" to="/leaderBoard">
+                  Leader Board
+                </NavLink>
+              </li>
+              <li className="nav-item">
                 <NavLink className="nav-link" to="/contact">
                   Contact
                 </NavLink>
