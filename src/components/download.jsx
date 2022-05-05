f60761d7c658fdcdb4acb25fd1e26e09be7e685d
Jullia Casimbon <ijcasimbon@gmail.coom>
2022-05-05 01:11:55 -0400

    edits

--- src/components/download.jsx
+++ src/components/download.jsx
@@ -6,24 +6,13 @@ function Download() {
     <div className="download">
       <div class="container">
         <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="https://github.com/ijCasimbon/capstone-site/raw/main/src/components/tired.jpg/"
-              alt=""
-              //width= "400"
-              //height = "400"
-            />
-          </div>
-          <div class="col-lg-5">
+          <div class="col-lg-12">
             <h1 class="font-weight-light">Download</h1>
             <p>
               Requirements to download the game.
               
             </p>
             <p><a href="https://onedrive.live.com/download?cid=D8CCFBE118E7FB56&resid=D8CCFBE118E7FB56%21106&authkey=AFUnJmDb-Jkq5k8" className="btn btn-dark btn-lg" role="button" aria-pressed="true" id="button" download>Game</a></p>
-            <p><a href="https://github.com/ijCasimbon/capstone-site/raw/main/src/components/tired.jpg/" className="btn btn-dark btn-lg" role="button" aria-pressed="true" id="button" download>tired</a></p>
-
           </div>
         </div>
       </div>
