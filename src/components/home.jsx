43428eb505bce4b2d9fa8d6aa815d76fedeed682
VictorCabrera-Tera <56519850+VictorCabrera-Tera@users.noreply.github.com>
2022-05-06 05:35:59 -0400

    edited contacts/home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,27 +1,29 @@
 import React from "react";
-
+//src="https://github.com/ijCasimbon/capstone-site/raw/main/src/components/download-logo.jpg/"
+//class="img-fluid rounded mb-4 mb-lg-0"
 function Home() {
   return (
     <div className="home">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
-            <p>
-              This is the game.
-            </p>
-          </div>
+    <h2 class="font-weight-light">Home</h2>
+    <div class="container">
+      <div class="row align-items-center my-5">
+        <div class="col-lg-7">
+          <img
+            class="img-fluid rounded mb-4 mb-lg-0"
+            src="https://github.com/ijCasimbon/capstone-site/raw/main/src/components/images/main-new.JPG"
+            alt=""
+          />
+        </div>
+        <div class="col-lg-5">
+          <h2 class="font-weight-light">Main-menu</h2>
+          <p>
+            This is where you start your adventure.
+          </p>
         </div>
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default Home;
