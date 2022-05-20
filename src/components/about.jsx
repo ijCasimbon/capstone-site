22bbc04cef879220556bea8244de860387f45fc3
Jullia Casimbon <ijcasimbon@gmail.com>
2022-05-20 01:05:16 -0400

    updated about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -29,17 +29,30 @@ function About() {
             <img src="https://github.com/ijCasimbon/capstone-site/blob/main/src/components/images/coin.png?raw=true" class="figure-img img-fluid rounded" className="coin" alt="coin" ></img>
             <figcaption>Players must collect all 3 coins.</figcaption>
           </figure>
+
+          <figure class="col-lg-12">
+            <img src="https://github.com/ijCasimbon/capstone-site/blob/main/src/components/images/enemies.png?raw=true" class="figure-img img-fluid rounded" className="coin" alt="coin" ></img>
+            <figcaption>Avoid all the enemies. If players touch them, they will lose 1 life.</figcaption>
+          </figure>
             
             <figure class="col-lg-12">
             <img src="https://github.com/ijCasimbon/capstone-site/blob/main/src/components/images/flag.png?raw=true" class="figure-img img-fluid rounded" className="flag" alt="flag" ></img>
             <figcaption>Once all coins are collected, go to the yellow flag to proceed to the next level.</figcaption>
           </figure>
+
           <figure class="col-lg-12">
-            <img src="https://github.com/ijCasimbon/capstone-site/blob/main/src/components/images/heart.png?raw=true" class="figure-img img-fluid rounded" className="heart" alt="heart" ></img>
+            <img src="https://github.com/ijCasimbon/capstone-site/blob/main/src/components/images/coin.png?raw=true" class="figure-img img-fluid rounded" className="coin" alt="Heart" ></img>
             <figcaption>Players must collect all 3 coins.</figcaption>
           </figure>
-            <p>  Complete all levels for a victory screen with your score :D</p>
-         
+          <figure class="col-lg-12">
+            <img src="https://github.com/ijCasimbon/capstone-site/blob/main/src/components/images/heart.png?raw=true" class="figure-img img-fluid rounded" className="heart" alt="heart" ></img>
+            <figcaption>Collect heart to regain 1 life that was lost.</figcaption>
+          </figure>
+          <figure class="col-lg-12">
+            <img src="https://github.com/ijCasimbon/capstone-site/blob/main/src/components/images/final.png?raw=true" class="figure-img img-fluid rounded" className="final" alt="final" ></img>
+            <figcaption>Finish all levels to complete the game.</figcaption>
+          </figure>
+          
             <h3><span className="lose">How to lose</span></h3>
             <p>  If you hit blue obstacles you lose HP.</p>
             <p>  Once your HP hits zero your game ends.</p>
@@ -60,17 +73,6 @@ function About() {
           <figure class="col-sm-12">
             <img src="https://github.com/ijCasimbon/capstone-site/blob/main/src/components/images/Immunity.png?raw=true" class="figure-img img-fluid rounded" alt="Immunity power-up image " ></img>
             <figcaption>Alters the color of specific walls & platforms to light blue. Allows the player to go through these selected areas</figcaption>
-          </figure>
-
-          <h3><span className="collectibles">Collectibles</span></h3>
-          <figure class="col-sm-12">
-            <img src="https://github.com/ijCasimbon/capstone-site/blob/main/src/components/images/heart.png?raw=true" class="figure-img img-fluid rounded" className="heart" alt="Heart" ></img>
-            <figcaption>Revives one life when collected</figcaption>
-          </figure>
-
-          <figure class="col-sm-12">
-            <img src="https://github.com/ijCasimbon/capstone-site/blob/main/src/components/images/coin.png?raw=true" class="figure-img img-fluid rounded" className="coin" alt="Heart" ></img>
-            <figcaption>Revives one life when collected</figcaption>
           </figure>
           </div>
         </div>
